5998dfa5d8f2c8526435ecad43358528fa8a0c34
Your_Name <joseph161216@gmail.com>
2021-05-27 19:50:05 -0700

    Add starter code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    // Make call to load data into state
+    fetch("https://api.github.com/orgs/Netflix/repos")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World</h1>
+      {
+        //map should return components to display info. Maybe have it display cards
+        data.map((repo) => {})
+      }
+    </>
   );
 }
 
