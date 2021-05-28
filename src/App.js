fbfea8bde99e6ebda542e8d07dc3acf04ead2968
Your_Name <joseph161216@gmail.com>
2021-05-27 20:13:14 -0700

    Display data on page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import RepoCard from './components/repoCard/RepoCard';
 
 function App() {
   const [data, setData] = useState([])
@@ -13,10 +14,17 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
       {
         //map should return components to display info. Maybe have it display cards
-        data.map((repo) => {})
+        data.map((repo) => (
+          <RepoCard
+            name={repo.name}
+            language={repo.language}
+            description={repo.description}
+            star_count={repo.stargazers_count}
+            fork_count={repo.forks}
+            date_created={repo.created_at}
+          />))
       }
     </>
   );
--- src/components/repoCard/RepoCard.jsx
+++ src/components/repoCard/RepoCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const RepoCard = () => {
+const RepoCard = ({
+  name,
+  language,
+  description,
+  star_count,
+  fork_count,
+  date_created,
+}) => {
   return (
-    <div>
-      
+    <div className="card">
+      <div>Name: {name}</div>
+      <div>Language: {language}</div>
+      <div>Description: {description}</div>
+      <div>Star Count: {star_count}</div>
+      <div>Fork Count: {fork_count}</div>
+      <div>Date Created: {date_created}</div>
+      <br />
     </div>
-  )
-}
+  );
+};
 
 export default RepoCard
